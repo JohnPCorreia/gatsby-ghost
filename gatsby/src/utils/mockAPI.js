0ce5c1869fc3c89f5b703a619bef2bc829905312
John Correia <johnc@fundopolis.com>
2019-08-15 13:20:39 -0400

    mend

---
+++ gatsby/src/utils/mockAPI.js
@@ -0,0 +1,504 @@
+export const investorFaq = [
+    {
+      title: "Can anyone invest in a Reg CF offering?",
+      content: [
+        {
+          paragraph: [
+            {
+              text:
+                "Yes, if you’re a U.S. citizen over the age of 18, you may participate in a Reg. CF raise.",
+              typeText: "normal",
+              color: "#6F7877"
+            }
+          ]
+        }
+      ]
+    },
+    {
+      title: "How much can I invest?",
+      content: [
+        {
+          paragraph: [
+            {
+              text:
+                "Like stocks and bonds, anyone can invest in crowdfunding offerings. But because of the risks involved, you are limited in how much",
+              typeText: "normal",
+              color: "#6F7877"
+            },
+            {
+              text:
+                "you can invest during any 12-month period with these kinds of securities. The inflation-adjusted investment limits depend on your net worth and annual income:",
+              typeText: "normal",
+              color: "#6F7877"
+            }
+          ]
+        },
+        {
+          paragraph: [
+            {
+              text:
+                " If either your annual income or your net worth is less than $107,000, then during any 12-month period, you can invest up to the greater of either $2,200 or five percent of the lesser of your annual income or net worth.",
+              typeText: "normal",
+              color: "#6F7877"
+            }
+          ]
+        },
+        {
+          paragraph: [
+            {
+              text:
+                " If both your annual income and your net worth are equal to or more than $107,000 then, during any 12-month period, you can invest up to 10 percent of your annual income or net worth, whichever is less, but not to exceed $107,000.",
+              typeText: "normal",
+              color: "#6F7877"
+            }
+          ]
+        },
+        {
+          paragraph: [
+            {
+              text:
+                "Say your annual income is $150,000 and your net worth is $80,000. JOBS Act crowdfunding rules allow you to invest the greater of $2,200 or five percent of $80,000 ($4,000) during a 12-month period. So, in this case, you can invest $4,000 over a 12-month period.",
+              typeText: "normal",
+              color: "#6F7877"
+            }
+          ]
+        }
+      ]
+    },
+    {
+      title: "What's an accredited investor?",
+      content: [
+        {
+          paragraph: [
+            {
+              text:
+                "An accredited investor is someone who has a net worth of at least $1 million (not including your home) or earned at least $200,000 in income the past two years and expects to again this year ($300,000 if combined with a spouse).",
+              typeText: "normal",
+              color: "#6F7877"
+            }
+          ]
+        }
+      ]
+    },
+    {
+      title: "How is net worth calculated?",
+      content: [
+        {
+          paragraph: [
+            {
+              text: "Net worth is the sum of all",
+              typeText: "normal",
+              color: "#6F7877"
+            },
+            {
+              text: "assests",
+              typeText: "blue",
+              tooltip: true,
+              tooltipMessage: [
+                {
+                  text: "has economic value",
+                  typeText: "normal"
+                }
+              ]
+            },
+            {
+              text: "minus all",
+              typeText: "normal",
+              color: "#6F7877"
+            },
+            {
+              text: "liabilities",
+              typeText: "blue",
+              spaceLastWord: false,
+              tooltip: true,
+              tooltipMessage: [
+                {
+                  text: "a debt or finanicial obligation",
+                  typeText: "normal"
+                }
+              ]
+            },
+            {
+              text:
+                ". In crowdfunding, the value of primary residence and the mortgage owed are not included in net worth calculations.",
+              typeText: "normal",
+              color: "#6F7877"
+            }
+          ]
+        },
+        {
+          paragraph: [
+            {
+              text:
+                "For additional information including calculations and examples, the",
+              typeText: "normal",
+              color: "#6F7877"
+            },
+            {
+              text: "SEC's Investor Bulletin Crowdfunding for Investors",
+              typeText: "link",
+              linkTo:
+                "https://www.sec.gov/oiea/investor-alerts-bulletins/ib_crowdfunding-.html",
+              target: "_blank"
+            },
+            {
+              text: "is super helpful!",
+              typeText: "normal",
+              color: "#6F7877"
+            }
+          ]
+        }
+      ]
+    },
+    {
+      title: "Can I invest if I'm not a U.S. Citizen?",
+      content: [
+        {
+          paragraph: [
+            {
+              text: "At this time, you must be a U.S. citizen to invest on",
+              typeText: "normal",
+              color: "#6F7877"
+            },
+            {
+              text: "Fund",
+              typeText: "bold",
+              spaceLastWord: false
+            },
+            {
+              text:
+                "opolis. We can also accommodate U.S. citizens with international addresses. In the future, we plan to offer our platform internationally.",
+              typeText: "normal",
+              color: "#6F7877"
+            }
+          ]
+        }
+      ]
+    },
+    {
+      title: "How do I invest on Fundopolis?",
+      content: [
+        {
+          paragraph: [
+            {
+              text: "Individuals can invest by first",
+              typeText: "normal",
+              color: "#6F7877"
+            },
+            {
+              text: "registering",
+              typeText: "link",
+              linkTo: "https://prod.fundopolis.com/citizen/createcitizen"
+            },
+            {
+              text: "a Citizen of Fundopolis.",
+              typeText: "normal",
+              color: "#6F7877"
+            },
+            {
+              text: "Already registered? Click",
+              typeText: "normal",
+              color: "#6F7877"
+            },
+            {
+              text: "here",
+              typeText: "link",
+              linkTo: "https://prod.fundopolis.com/"
+            },
+            {
+              text: "to log in to your account.",
+              typeText: "normal",
+              color: "#6F7877"
+            }
+          ]
+        },
+        {
+          paragraph: [
+            {
+              text: "Once logged in, navigate to the",
+              typeText: "normal",
+              color: "#6F7877"
+            },
+            {
+              text: "Invest Page",
+              typeText: "link",
+              linkTo: "https://prod.fundopolis.com/invested"
+            },
+            {
+              text:
+                "from the menu, browse to our live raises and find one you're interested in supporting. Click the blue,",
+              typeText: "normal",
+              color: "#6F7877"
+            },
+            {
+              text: "Funderize",
+              typeText: "italics",
+              color: "#6F7877"
+            },
+            {
+              text:
+                "button and fill out the form to complete your investment. Now all you need to do is enter your payment information, sign the form and you're good to go.",
+              typeText: "normal",
+              color: "#6F7877"
+            }
+          ]
+        },
+        {
+          paragraph: [
+            {
+              text:
+                "You'll immediately receive a confirmation email from our Fundsquad. If your investment requires further action, the Fundsquad will contact you directly.",
+              typeText: "normal",
+              color: "#6F7877"
+            }
+          ]
+        }
+      ]
+    },
+    {
+      title: "What if the company I invest into fails to raise enough money?",
+      content: [
+        {
+          paragraph: [
+            {
+              text:
+                "If the company doesn't raise the amount of money it says it needs to collect as a minimum investment, your investment will be canceled, and your money will be returned to you.",
+              typeText: "normal",
+              color: "#6F7877"
+            }
+          ]
+        }
+      ]
+    },
+    {
+      title: "What if I change my mind?",
+      content: [
+        {
+          paragraph: [
+            {
+              text: " If you change your mind, you will have 48 hours prior to a",
+              typeText: "normal",
+              color: "#6F7877"
+            },
+            {
+              text: "raise close",
+              typeText: "link",
+              linkTo: "https://prod.fundopolis.com/faq#glossary"
+            },
+            {
+              text: "or",
+              typeText: "normal",
+              color: "#6F7877"
+            },
+            {
+              text: "rolling close",
+              typeText: "link",
+              linkTo: "https://prod.fundopolis.com/faq#glossary"
+            },
+            {
+              text:
+                "to cancel your investment. If you cancel your investment before the 48-hour deadline,",
+              typeText: "normal",
+              color: "#6F7877"
+            },
+            {
+              text: "Fund",
+              typeText: "bold",
+              spaceLastWord: false,
+              color: "#6F7877"
+            },
+            {
+              text: "opolis will arrange with the",
+              typeText: "normal",
+              color: "#6F7877"
+            },
+            {
+              text: "escrow agent",
+              typeText: "link",
+              linkTo: "https://prod.fundopolis.com/faq#glossary"
+            },
+            {
+              text: "for the return of your money.",
+              typeText: "normal",
+              color: "#6F7877"
+            }
+          ]
+        }
+      ]
+    },
+    {
+      title: "What's a Raise Close?",
+      content: [
+        {
+          paragraph: [
+            {
+              text:
+                "It's like a closing on your house; it's the moment when everything becomes official. You are issued a security or a pending security in exchange for your money which has been held in",
+              typeText: "normal",
+              color: "#6F7877"
+            },
+            {
+              text: "escrow",
+              typeText: "blue",
+              tooltip: true,
+              tooltipMessage: [
+                {
+                  text: "funds are held by a third party",
+                  typeText: "normal"
+                }
+              ]
+            },
+            {
+              text:
+                "prior to this point. At raise close, the money is transferred to the business conducting the raise, and the investor is issued a security.",
+              typeText: "normal",
+              color: "#6F7877"
+            }
+          ]
+        }
+      ]
+    },
+    {
+      title: "What's a Rolling Close?",
+      content: [
+        {
+          paragraph: [
+            {
+              text:
+                "If a business reaches its minimum fundraising goal amount, it can close on those investments and continue to raise to its maximum fundraising goal amount. For example, if a company's minimum raise is $50,000 and its maximum raise is $500,000, once it hits $50,000, the company can close on that amount and continue to raise toward its $500,000 maximum. Again, you have 48 hours prior to any close, rolling or otherwise, to cancel your investment.",
+              typeText: "normal",
+              color: "#6F7877"
+            }
+          ]
+        }
+      ]
+    },
+    {
+      title: "Where is the proof of my investment?",
+      content: [
+        {
+          paragraph: [
+            {
+              text:
+                " After you make an investment, you will receive an email confirming your investment. You will also recive an email showing that your investment funds have been received by the escrow agent.  The Fundopolis system will also provide you with evidence of your investment after a close.",
+              typeText: "normal",
+              color: "#6F7877"
+            }
+          ]
+        }
+      ]
+    },
+    {
+      title: "Do Funders pay any fees?",
+      content: [
+        {
+          paragraph: [
+            {
+              text:
+                "Investors will be charged 2% of the amount invested with a minimum fee of $3.50 and a maximum fee of $75.00. This fee, less a charge of $2.00 will be refunded in the case where a company doesn’t meet its stated minimum fundraising goal. ",
+              typeText: "normal",
+              color: "#6F7877"
+            },
+            {
+              text: "",
+              color: "blue",
+              tooltip: true,
+              tooltipMessage: [
+                {
+                  text: "investors",
+                  typeText: "normal"
+                }
+              ]
+            },
+            {
+              text: "",
+              typeText: "normal",
+              color: "#6F7877"
+            },
+            {
+              text: "",
+              typeText: "bold",
+              spaceLastWord: false,
+              color: "#6F7877"
+            },
+            {
+              text: "",
+              typeText: "normal",
+              color: "#6F7877"
+            }
+          ]
+        }
+      ]
+    },
+    {
+      title:
+        "What are the different types of security investments Fundopolis offers?",
+      content: [
+        {
+          paragraph: [
+            {
+              text: "Equity:",
+              typeText: "bold",
+              spaceLastWord: true,
+              color: "#6F7877"
+            },
+            {
+              text:
+                "Equity represents an ownership in the business.  Depending on the type of equity issued, the security may include a claim on part of the business’s assets and earnings or even voting rights.  Offering equity is a great way to raise money quickly without needing to pay that money back.  However, be sure you’re comfortable relinquishing some control and ownership of your business.",
+              typeText: "normal",
+              color: "#6F7877"
+            }
+          ]
+        },
+        {
+          paragraph: [
+            {
+              text: "Debt security:",
+              typeText: "bold",
+              spaceLastWord: true,
+              color: "#6F7877"
+            },
+            {
+              text:
+                "A debt security is similar to a standard loan, like your car loan.  It can be bought and sold between two parties and basic terms such as amount borrowed, interest rate and loan repayment date are determined up front.  Debt securities are attractive to businesses who want to retain full control of the company and believe they’ll be able to not only repay their loans, but on time.",
+              typeText: "normal",
+              color: "#6F7877"
+            }
+          ]
+        },
+        {
+          paragraph: [
+            {
+              text: "Revenue Share:",
+              typeText: "bold",
+              spaceLastWord: true,
+              color: "#6F7877"
+            },
+            {
+              text:
+                "Revenue shares are very similar to debt securities.  The main difference is that interest rate and the loan repayment date depend on how well the business does.  Businesses pay back a percent of net revenue each month until the loan is paid in full.  When the business is doing well, a funder may receive larger payments up front; when the business is slow, payments will be lower and may take a longer time to be paid (especially if there is no stated repayment date).  Offering revenue shares allow you to retain full control of your company and are favorable to businesses who can't support a fixed payment schedule.",
+              typeText: "normal",
+              color: "#6F7877"
+            }
+          ]
+        },
+        {
+          paragraph: [
+            {
+              text: "Convertible Note:",
+              typeText: "bold",
+              spaceLastWord: true,
+              color: "#6F7877"
+            },
+            {
+              text:
+                "A convertible note is similar to a debt security in that it's initially a standard loan agreement. The difference is that the loan accrues interest which may either be paid out at end of the loan or converted into equity based on a predetermined discount rate. A convertible note typically pays out at a reduced interest rate due to the potential upside of converting to equity. In the event that the note is “converted,” the amount of equity you receive will depend on the specific terms of the note.  Convertible notes are common if seeking immediate capital, flexible financing options and lower interest rates. Keep in mind that if the note converts to equity, businesses are responsible for investor reporting.",
+              typeText: "normal",
+              color: "#6F7877"
+            }
+          ]
+        }
+      ]
+    }
+  ];
+  
